928a95c7332b65633664a0e0c0cc2ae895e15cd3
NagendharanNagu <nagendharan.shanmugam@gmail.com>
2025-08-12 09:17:37 +0530

    Day - 12 Edit and update handler functions included

--- onTrack/src/components/Crud.jsx
+++ onTrack/src/components/Crud.jsx
@@ -4,6 +4,9 @@ const Crud = () => {
   // State initialization
   const [tasks, setTasks] = useState([]); // To handle the values as an array
   const [current, setCurrent] = useState(""); // To hold the value of the input field
+  const [isEditable, setIseditable] = useState(false) // To conditionally render the Add/Update button
+  const [editedValue, setEditedvalue] = useState("") // To store the updated value
+
 
   // Onchange Event
   const handleChange = (e) => {
@@ -11,27 +14,43 @@ const Crud = () => {
     setCurrent(e.target.value);
   };
 
-  // Creating
+  // Creating - addhandler
   const addTask = (e) => {
     e.preventDefault();
     setTasks([...tasks, {id: Date.now(), text:current}]);
-    console.log(tasks);
     setCurrent("");
   };
+  // console.log(tasks);
+
+  //Reading - Edithandler
+  const handleEdit = (task) =>{
+    // console.log(task)
+    setCurrent(task.text)
+    setIseditable(true)
+    setEditedvalue(task)
+    // console.log(task)
+  }
+  // console.log(editedValue)
+
+  //Updating - Updatehandler
+  const handleUpdate = () =>{
+    setIseditable(false)
+    setCurrent("")
+  }
 
   return (
     <div>
       <div>
         {/* passing the name and value prop to the input field */}
         <input type="text" name={current} value={current} onChange={handleChange} placeholder="Enter your task..!"/>
-        <button onClick={addTask}>Add</button>
+        {isEditable ? (<button onClick={handleUpdate}>Update</button>):(<button onClick={addTask}>Add</button>)}
       </div>
       <div>
        <ul>
         {tasks.map((task)=>(
             <li key={task.id} type="none">
                 <span>{task.text}</span>
-                <button>Edit</button>
+                <button onClick={() => handleEdit(task)}>Edit</button>
                 <button>Delete</button>
             </li>
         ))}
