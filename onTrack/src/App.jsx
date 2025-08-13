e5040f90e60bf9f09bb28d6482d2cc9fcefa6c78
NagendharanNagu <nagendharan.shanmugam@gmail.com>
2025-08-13 23:00:40 +0530

    Day - 13 Included Styling

--- onTrack/src/App.jsx
+++ onTrack/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Crud from "./components/Crud";
+import Copy from "./components/Copy";
 
 const App = ()=>{
     return(
         <div>
             <Crud/>
+            {/* <Copy/> */}
         </div>
     )
 }
--- onTrack/src/components/Crud.jsx
+++ onTrack/src/components/Crud.jsx
@@ -1,54 +1,111 @@
 import React, { useEffect, useState } from "react";
+import "../css/crud.css";
 
 const Crud = () => {
   // State initialization
   const [inputValue, setInputValue] = useState("");
   const [tasks, setTasks] = useState([]);
   const [selectedTask, setSelectedTask] = useState(null);
+  const [completedTask, setCompletedTask] = useState([]);
+  const [currentTab, setCurrentTab] = useState("PENDING");
 
   const addtaskHandler = () => {
     const transformedData = {
       id: Date.now(),
       text: inputValue,
+      isCompleted: false,
     };
     setTasks([...tasks, transformedData]);
     setInputValue("");
   };
 
-  const editTaskHandler = (task)=>{
-    setInputValue(task.text)
-    setSelectedTask(task)
-  }
+  const editTaskHandler = (task) => {
+    setInputValue(task.text);
+    setSelectedTask(task);
+  };
 
-  const updateTaskHandler = ()=>{
-    const tempArr = [...tasks]
-    const currentValueIndex = tempArr.findIndex(p => p.id == selectedTask.id)
-    const currentData = tempArr[currentValueIndex]
-    currentData.text = inputValue
+  const updateTaskHandler = () => {
+    const tempArr = [...tasks];
+    const currentValueIndex = tempArr.findIndex((p) => p.id == selectedTask.id);
+    const currentData = tempArr[currentValueIndex];
+    currentData.text = inputValue;
 
-    setTasks(tempArr)
-    setSelectedTask(null)
-    setInputValue("")
-  }
- 
-  const deleteTaskHandler = (task)=>{
-    const filteredTasks = tasks.filter(p => p.id != task.id)
-    setTasks(filteredTasks)
-  }
+    setTasks(tempArr);
+    setSelectedTask(null);
+    setInputValue("");
+  };
+
+  const deleteTaskHandler = (task) => {
+    const filteredTasks = tasks.filter((p) => p.id != task.id);
+    setTasks(filteredTasks);
+  };
+
+  const checkboxHandler = (task) => {
+    const filteredTasks = tasks.filter((p) => p.id == task.id);
+    setCompletedTask(filteredTasks);
+    deleteTaskHandler(task);
+  };
+
+  const completedHandler = () => {
+    return completedTask.map((item) => (
+      <ul key={item.id} type="none">
+        <li>
+          <span>{item.text}</span>
+        </li>
+      </ul>
+    ));
+  };
 
   return (
-    <div>
-      <input type="text" name="text-box" value={inputValue} onChange={(event) => setInputValue(event.target.value)}/>
-      {selectedTask ? <button onClick={updateTaskHandler}>Update</button> : <button onClick={addtaskHandler}>Add</button> }
-      {tasks.map((item) => (
-        <ul type="none" key={item.id}>
-          <li>
-            <span>{item.text}</span>
-            <button onClick={()=> editTaskHandler(item)}>Edit</button>
-            <button onClick={()=> deleteTaskHandler(item)}>Delete</button>
-          </li>
-        </ul>
-      ))}
+    <div className="task-container">
+      <h1 className="text">Action Tracker</h1>
+      <div className="input-and-btn-container">
+        <input
+          type="text"
+          name="text-box"
+          value={inputValue}
+          onChange={(event) => setInputValue(event.target.value)}
+          className="task-text-input"
+        />
+        {selectedTask ? (
+          <button onClick={updateTaskHandler} className="update-btn">
+            UpdateTask
+          </button>
+        ) : (
+          <button onClick={addtaskHandler} className="add-btn">
+            AddTask
+          </button>
+        )}
+      </div>
+      <div>
+       <div className="task-tabs">
+         <div>
+          <button onClick={() => setCurrentTab("PENDING")} className={currentTab === "PENDING" ? "active-tab" : "inactive-tab"}>ToDo</button>
+        </div>
+        <div>
+          <button onClick={() => setCurrentTab("COMPLETED")} className={currentTab === "COMPLETED" ? "active-tab" : "inactive-tab"}>Completed</button>
+        </div>
+       </div>
+        
+          {currentTab == "PENDING"
+            ? tasks.map((item) => (
+                <ul type="none" key={item.id}>
+                  <li>
+                    <input
+                      type="checkbox"
+                      onClick={() => checkboxHandler(item)}
+                    />
+                    <span>{item.text}</span>
+                    <button onClick={() => editTaskHandler(item)}>Edit</button>
+                    <button onClick={() => deleteTaskHandler(item)}>
+                      Delete
+                    </button>
+                  </li>
+                </ul>
+              ))
+            : completedHandler()}
+        
+      </div>
     </div>
   );
 };
